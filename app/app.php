5e025c8c275591700177556ac9e3318ad26e6e9e
Ike Mattice & Ashlin Aronin <student@epicodus.com>
2015-08-04 15:21:21 -0700

    Add JobOpening class

--- app/app.php
+++ app/app.php
@@ -1,10 +1,14 @@
 <?php
-    require_once __DIR__"/../vendor/autoload.php";
+    require_once __DIR__."/../vendor/autoload.php";
 
     $app = new Silex\Application();
 
     $app->get("/", function() {
-        return "Poor?  Click here!";
+        return "<h1><a href='/carworld-search'>Poor?  Click here!</a></h1>";
+    });
+
+    $app->get("/Jobs-for-sale", function() {
+
     });
 
     return $app;
---
+++ src/JobOpening.php
@@ -0,0 +1,30 @@
+<?php
+
+class JobOpening {
+    private $title;
+    private $descript;
+    private $pay;
+    private $contact;
+
+    function setPay($new_pay) {
+        $this->pay = (float) $new_pay;
+    }
+
+/*    function setContact($new_contact) {
+        $this->contact =
+    }
+*/
+
+    function getTitle()
+        {
+            return $this->title;
+        }
+    function getDescript() {
+        return $this->descript;
+    }
+    function getPay(){
+        return $this->pay;
+    }
+    function getContact(){
+        return $this->contact;
+    }
